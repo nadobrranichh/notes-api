32ecff60654ec7f2fff3ab24ad8f0ddbafed7c32
nadobrranichh <nazarshch26@gmail.com>
2026-02-17 19:07:47 +0200

    feat: added DTO for notes, mapping between dtos and entities for presentation layer

---
+++ src/main/java/com/nadobranich/notes_api/domain/NoteDto.java
@@ -0,0 +1,14 @@
+package com.nadobranich.notes_api.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NoteDto {
+    private Integer id;
+
+    private String content;
+}
---
+++ src/main/java/com/nadobranich/notes_api/mapping/NoteMapper.java
@@ -0,0 +1,24 @@
+package com.nadobranich.notes_api.mapping;
+
+import com.nadobranich.notes_api.domain.NoteDto;
+import com.nadobranich.notes_api.domain.NoteEntity;
+import org.modelmapper.ModelMapper;
+
+public class NoteMapper implements Mapper<NoteEntity, NoteDto> {
+
+    private ModelMapper modelMapper;
+
+    public NoteMapper() {
+        this.modelMapper = new ModelMapper();
+    }
+
+    @Override
+    public NoteDto mapTo(NoteEntity noteEntity) {
+        return modelMapper.map(noteEntity, NoteDto.class);
+    }
+
+    @Override
+    public NoteEntity mapFrom(NoteDto noteDto) {
+        return modelMapper.map(noteDto, NoteEntity.class);
+    }
+}
